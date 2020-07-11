74276ade95a3034e54e5f7bb2f38ea22efc80cec
norberto <norberto.a.vega@gmail.com>
2020-07-11 00:01:29 -0300

    Cambia el linearLayoutManager del recyclerView por gridLayoutManayer y se adapta list_item_sleep_night al nuevo layoutmanager

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -31,6 +32,8 @@ class SleepTrackerFragment : Fragment() {
         binding.sleepTrackerViewModel = sleepTrackerViewModel
         binding.setLifecycleOwner(this)
 
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
         val adapter = SleepNightAdapter()
         binding.sleepList.adapter = adapter
 
